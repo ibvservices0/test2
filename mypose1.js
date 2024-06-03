bf2be6cb9ded3a15c67468d035c26163588de65f
ibvservices0 <ibvservices0@gmail.com>
2024-06-03 12:36:04 +0200

    siguiente83

--- mypose1.js
+++ mypose1.js
@@ -87,6 +87,8 @@ function verEnCanvas(img){
     if(clockwise){ctx.translate(ih, 0);
     } else {ctx.translate(0, iw);}
     ctx.rotate(degrees*Math.PI/180);
+    ctx.clearRect(0, 0, mycanvas.width, mycanvas.height);
+    ctx.drawImage(img, 0, 0);
 
 
     const the_dataURL = mycanvas.toDataURL('image/jpeg', 1.0);;
